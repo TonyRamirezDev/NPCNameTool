28ca7b61b40da5f57ef3b7e6f71a8c8611434896
Anthony Ramirez <tramirez1992@gmail.com>
2018-02-20 12:10:55 -0500

    Adding functionality to button click

--- app/src/main/java/tonyram/npcnametool/MainActivity.java
+++ app/src/main/java/tonyram/npcnametool/MainActivity.java
@@ -1,13 +1,37 @@
 package tonyram.npcnametool;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    @Override
+//    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
+
+
+    private void init() {
+        Button randomizeButton = (Button)findViewById(R.id.randomizeButton);
+        randomizeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView nameValue = (TextView)findViewById(R.id.nameValue);
+                nameValue.setText("New Name");
+            }
+        });
+
+    }
+
+
+
+
 }
+
+
