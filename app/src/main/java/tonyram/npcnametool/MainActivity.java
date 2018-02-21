14100d4f2692a0b318e54b1e887aa22bd10b3c0b
Anthony Ramirez <tramirez1992@gmail.com>
2018-02-21 15:40:26 -0500

    Making class variables and adding basic logic for list view

--- app/src/main/java/tonyram/npcnametool/MainActivity.java
+++ app/src/main/java/tonyram/npcnametool/MainActivity.java
@@ -5,19 +5,44 @@ import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends Activity {
 
-//    @Override
+
+    InputStreamReader firstNameStream;
+    InputStreamReader lastNameStream;
+    BufferedReader brFirstName;
+    BufferedReader brLastName;
+    int numLinesFirstName;
+    int numLinesLastName;
+    Random randomFirstName;
+    Random randomLastName;
+    int desiredLineFirstName;
+    int desiredLineLastName;
+    String newFirstName;
+    String newLastName;
+    int lineCounterFirstName;
+    int lineCounterLastName;
+    StringBuilder fullName;
+
+    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -33,23 +58,22 @@ public class MainActivity extends Activity {
         randomizeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                InputStreamReader firstNameStream = new InputStreamReader(getResources().openRawResource(R.raw.firstnames));
-                InputStreamReader lastNameStream = new InputStreamReader(getResources().openRawResource(R.raw.lastnames));
-                BufferedReader brFirstName = new BufferedReader(firstNameStream);
-                BufferedReader brLastName = new BufferedReader(lastNameStream);
-                int numLinesFirstName = 100;
-                int numLinesLastName = 70;
-                Random randomFirstName = new Random();
-                Random randomLastName = new Random();
-                int desiredLineFirstName = randomFirstName.nextInt(numLinesFirstName);
-                int desiredLineLastName = randomLastName.nextInt(numLinesLastName);
+                firstNameStream = new InputStreamReader(getResources().openRawResource(R.raw.firstnames));
+                lastNameStream = new InputStreamReader(getResources().openRawResource(R.raw.lastnames));
+                brFirstName = new BufferedReader(firstNameStream);
+                brLastName = new BufferedReader(lastNameStream);
+                numLinesFirstName = 100;
+                numLinesLastName = 70;
+                randomFirstName = new Random();
+                randomLastName = new Random();
+                desiredLineFirstName = randomFirstName.nextInt(numLinesFirstName);
+                desiredLineLastName = randomLastName.nextInt(numLinesLastName);
+                newFirstName = "";
+                newLastName = "";
+                lineCounterFirstName = 0;
+                lineCounterLastName = 0;
+                fullName = new StringBuilder();
 
-                String newFirstName = "";
-                String newLastName = "";
-                int lineCounterFirstName = 0;
-                int lineCounterLastName = 0;
-
-                StringBuilder fullName = new StringBuilder();
 
                 try {
                     while ((newFirstName = brFirstName.readLine()) != null){
@@ -67,13 +91,40 @@ public class MainActivity extends Activity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                //newNameText.setText(newFirstName + " " + newLastName);
                 fullName.append(newFirstName);
                 fullName.append(" ");
                 fullName.append(newLastName);
                 newNameText.setText(fullName);
             }
         });
+
+
+        ListView savedList = (ListView)findViewById(R.id.listSaved);
+        Button saveButton = (Button)findViewById(R.id.saveButton);
+        final EditText descriptionText = (EditText)findViewById(R.id.descriptionInput);
+        final EditText locationText = (EditText)findViewById(R.id.locationInput);
+        final ArrayList<String> listItems = new ArrayList<String>();
+        final ArrayAdapter<String> stringAdapter = new ArrayAdapter<String>(this, R.layout.saved_list, listItems);
+//        savedList.setAdapter(stringAdapter);
+
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                listItems.add(fullName.toString());
+                listItems.add(descriptionText.getText().toString());
+                listItems.add(locationText.getText().toString());
+                stringAdapter.notifyDataSetChanged();
+            }
+
+        });
+
+//        savedList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+//                //do something
+//                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_LONG).show();
+//            }
+//        });
+
 
     }
 
